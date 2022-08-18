14024ce9ed5cb7fed6132f9d2db102b8ff126761
ruben orgaz <ruben.orgaz@inputforyou.es>
2022-08-18 08:56:29 +0200

    Inital Repo for databases

---
+++ onBoard/onBoard/DBRepo/DBRepoSQL.cs
@@ -0,0 +1,27 @@
+﻿using onBoard.Models;
+
+namespace onBoard.DBRepo
+{
+    public class DBRepoSQL : IDBRepo<Hour>
+    {
+        public Task<List<Hour>> AsyncGetList(int? offset = null, int? limit = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AsyncStoreTimeSpan(TimeSpan currentHour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Hour> GetList(int? offset = null, int? limit = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool StoreTimeSpan(TimeSpan currentHour)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ onBoard/onBoard/DBRepo/IDBRepo.cs
@@ -0,0 +1,11 @@
+﻿namespace onBoard.DBRepo
+{
+    public interface IDBRepo<T>
+    {
+        public bool StoreTimeSpan(TimeSpan currentHour);
+        public Task<bool> AsyncStoreTimeSpan(TimeSpan currentHour);
+
+        public List<T> GetList(int? offset = null, int? limit = null);
+        public Task<List<T>> AsyncGetList(int? offset = null, int? limit = null);
+    }
+}

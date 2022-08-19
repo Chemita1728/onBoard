176a6506b91f407838e1cd770c344d9d00eb5763
ruben orgaz <ruben.orgaz@inputforyou.es>
2022-08-19 10:57:02 +0200

    Added BsonId to primaryKey

--- onBoard/onBoard/Models/HourMongo.cs
+++ onBoard/onBoard/Models/HourMongo.cs
@@ -1,7 +1,12 @@
-﻿namespace onBoard.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace onBoard.Models
 {
     public class HourMongo : Hour
     {
-        private object _id;
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public object _id;
     }
 }
